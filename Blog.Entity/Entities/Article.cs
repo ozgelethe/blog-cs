5fce565d8cf2d4f6ed94e42a7742621855076fee
ozge.arican <ozge.arican@diasteknoloji.com>
2025-01-26 20:19:24 +0300

    first commit for blog

---
+++ Blog.Entity/Entities/Article.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Entity.Entities
+{
+    public class Article
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int ViewContent { get; set; }
+        public Category Category { get; set; }
+        public Guid CategoryId { get; set; }
+        public Image Image { get; set; }
+        public Guid ImageId { get; set; }
+    }
+}
