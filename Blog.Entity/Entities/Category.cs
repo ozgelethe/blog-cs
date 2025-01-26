ae9acbbaa907f05f280b19204221dce324b1596c
ozge.arican <ozge.arican@diasteknoloji.com>
2025-01-27 00:29:52 +0300

    removed IEntityBase from entities because EntityBase already inherits IEntityBase

--- Blog.Entity/Entities/Category.cs
+++ Blog.Entity/Entities/Category.cs
@@ -2,7 +2,7 @@
 
 namespace Blog.Entity.Entities
 {
-    public class Category : EntityBase, IEntityBase
+    public class Category : EntityBase
     {
         public string Name { get; set; }
         public ICollection<Article> Articles { get; set; }
